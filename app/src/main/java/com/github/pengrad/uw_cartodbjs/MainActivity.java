fd0ae6ef4a11e7e39a5554f56409505181b2e5c1
Stas Parshin <s.a.parshin@gmail.com>
2015-11-11 22:41:14 +0300

    Add Google maps

--- app/src/main/java/com/github/pengrad/uw_cartodbjs/MainActivity.java
+++ app/src/main/java/com/github/pengrad/uw_cartodbjs/MainActivity.java
@@ -1,13 +1,44 @@
 package com.github.pengrad.uw_cartodbjs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity {
+
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                setUpMap(googleMap);
+            }
+        });
+    }
+
+    private void setUpMap(GoogleMap googleMap) {
+        mMap = googleMap;
+//        mMap.setOnMarkerClickListener(this);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(36.0840889, -79.7401672), 10));
+    }
+
+
+    void a() {
+        String url = "https://buildingupdreams.cartodb.com/api/v2/sql?q=select%20*%20from%20public.guilford_county_area_parks";
     }
 }
